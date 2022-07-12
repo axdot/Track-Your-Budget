d0f176f827aa3b214850c96613ab70ae966e828c
Chris <kimballc3@yahoo.com>
2022-07-12 01:55:17 -0500

    add manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,10 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-  "./public/index.html",
-  "./public/css/styles.css",
-  "./public/js/index.js",
-  "./public/js/idb.js"
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js"
 ];
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
